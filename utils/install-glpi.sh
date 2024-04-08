7d34cb1708d2be21bc10eb416326a0520dbd02eb
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2024-04-08 16:31:54 -0300

    Melhorando o script

--- utils/install-glpi.sh
+++ utils/install-glpi.sh
@@ -5,9 +5,12 @@ tar -xvzf glpi-10.0.14.tgz
 cd $(pwd)/glpi
 rm -rf /var/www/html/*
 mv * /var/www/html
+chmod 777 /var/www/html/config -R
+chmod 777 /var/www/html/files/ -R
 apt-get install php -y;
 apt-get install php-mysql -y;
 apt-get install php-dom -y;
 apt-get install php-xml -y;
 apt-get install php-curl -y;
 apt-get install php-gd -y;
+systemctl restart apache2.service
