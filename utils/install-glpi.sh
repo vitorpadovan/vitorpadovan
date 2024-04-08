e431609bb45d789ae21d57cf8c946942f0e60fe8
Vitor Hugo Padovan Hernandes <vitor.padovan89@gmail.com>
2024-04-08 16:11:42 -0300

    Melhorando script

--- utils/install-glpi.sh
+++ utils/install-glpi.sh
@@ -1,4 +1,10 @@
 #/bin/sh
+#wget https://raw.githubusercontent.com/vitorpadovan/vitorpadovan/main/utils/install-glpi.sh
+wget https://github.com/glpi-project/glpi/releases/download/10.0.14/glpi-10.0.14.tgz
+tar -xvzf glpi.tar
+cd glpi
+mv * -r /var/www/html
+rm /var/www/html/index.html
 apt-get install php -y;
 apt-get install php-mysql -y;
 apt-get install php-dom -y;
